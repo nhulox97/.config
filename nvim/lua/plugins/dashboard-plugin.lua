5c9fcca1d16e8394c1298b0e4e1d6b1f1c5044a5
Sergio Bernal <sergio.bernal@telus.com>
2024-01-05 13:20:47 -0600

    chore: use doom theme

--- nvim/lua/plugins/dashboard-plugin.lua
+++ nvim/lua/plugins/dashboard-plugin.lua
@@ -2,35 +2,148 @@ return {
   'glepnir/dashboard-nvim',
   event = 'VimEnter',
   config = function()
-    require('dashboard').setup {
-      disable_move = true,
-      shorcut_type = 'number',
-      theme = 'hyper',
-      config = {
-        week_header = {
-          enable = true,
-        },
-        shortcut = {
-          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
-          {
-            icon = ' ',
-            icon_hl = '@variable',
-            desc = 'Files',
-            group = 'Label',
-            action = 'Telescope find_files',
-            key = 'f',
-          },
-          {
-            icon = '󰙅 ',
-            icon_hl = '@variable',
-            desc = 'Tree',
-            group = 'Label',
-            action = 'NvimTreeOpen',
-            key = 't',
-          },
-        },
-      },
+    -- local hyper_conf = {
+    --   disable_move = true,
+    --   shorcut_type = 'number',
+    --   theme = 'hyper',
+    --   config = {
+    --     week_header = {
+    --       enable = true,
+    --     },
+    --     shortcut = {
+    --       { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+    --       {
+    --         icon = ' ',
+    --         icon_hl = '@variable',
+    --         desc = 'Files',
+    --         group = 'Label',
+    --         action = 'Telescope find_files',
+    --         key = 'f',
+    --       },
+    --       {
+    --         icon = '󱏒 ',
+    --         icon_hl = '@variable',
+    --         desc = 'Tree',
+    --         group = 'Label',
+    --         action = 'NvimTreeOpen',
+    --         key = 't',
+    --       },
+    --     },
+    --   },
+    -- }
+
+    local dashboard_custom_header9 = {
+      '',
+      ' ⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷ ',
+      ' ⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇ ',
+      ' ⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽ ',
+      ' ⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕ ',
+      ' ⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕ ',
+      ' ⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕ ',
+      ' ⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄ ',
+      ' ⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ',
+      ' ⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿ ',
+      ' ⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿ ',
+      ' ⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⣿ ',
+      ' ⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠ ',
+      ' ⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙ ',
+      ' ⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡣ ',
+      '',
     }
+
+    local dashboard_custom_header5 = {
+      '',
+      '',
+      '        ⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀  ',
+      '      ⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷ ',
+      '     ⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿ ',
+      '    ⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿⣿ ',
+      '  ⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿⣿ ',
+      ' ⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿⣿ ',
+      ' ⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟ ',
+      ' ⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣ ',
+      ' ⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⣿⣿⣷ ',
+      ' ⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿ ',
+      ' ⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿ ',
+      ' ⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿ ',
+      ' ⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿ ',
+      '  ⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋  ⣾⡌⢠⣿⡿⠃ ',
+      ' ⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉            ',
+      '',
+      '',
+    }
+
+
+    local doom_conf = {
+      theme = 'doom',
+      config = {
+        header = dashboard_custom_header5, --your footer
+        center = {
+          {
+            icon = ' \t',
+            desc = 'Lazy update              ',
+            key = 'u',
+            action = 'Lazy update'
+          },
+          {
+            icon = '󰓅 \t',
+            desc = 'Lazy profile              ',
+            key = 'p',
+            action = 'Lazy profile'
+          },
+          {
+            icon = ' \t',
+            desc = 'Find files           ',
+            key = 'f',
+            action = 'Telescope find_files'
+          },
+          {
+            icon = '󰈞 \t',
+            desc = 'Find into files           ',
+            key = 'g',
+            action = 'Telescope live_grep'
+          },
+          {
+            icon = '󱏒 \t',
+            desc = 'Open tree             ',
+            key = 't',
+            action = 'NvimTreeOpen'
+          },
+          {
+            icon = ' \t',
+            desc = 'Neogit              ',
+            key = 'n',
+            action = 'Neogit'
+          },
+          {
+            icon = ' \t',
+            desc = 'Find changes',
+            key = 'd',
+            action = 'Telescope git_status'
+          },
+          {
+            icon = ' \t',
+            desc = 'Find branches',
+            key = 'b',
+            action = 'Telescope git_branches'
+          },
+          {
+            icon = ' \t',
+            desc = "Change color             ",
+            key = 'c',
+            action = 'Telescope colorscheme'
+          },
+          {
+            icon = ' \t',
+            desc = 'Quit               ',
+            key = 'q',
+            action = 'quit'
+          }
+        },
+      }
+    }
+
+    require('dashboard').setup(doom_conf)
   end,
   dependencies = { { 'nvim-tree/nvim-web-devicons' } }
 }
