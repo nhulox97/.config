843282d5a2e6d8b8527a66bbb7220e6c59d03b21
Sergio Bernal <sergio.bernal@telus.com>
2024-01-12 13:36:46 -0600

    refactor: remove char printed on tabs/spaces

--- nvim/lua/plugins/simple-indent-plugin.lua
+++ nvim/lua/plugins/simple-indent-plugin.lua
@@ -2,6 +2,7 @@ return {
   'lucastavaresa/simpleIndentGuides.nvim',
   config = function()
     vim.opt.list = true -- enable in all buffers
-    require("simpleIndentGuides").setup("│", "·")
+    -- require("simpleIndentGuides").setup("│", "·")
+    require("simpleIndentGuides").setup("│", " ")
   end
 }
