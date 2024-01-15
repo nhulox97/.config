1bfcdcfda080e645c08a4c4ffcb668da78b0b043
Sergio Bernal <sergio.bernal@telus.com>
2024-01-15 09:53:41 -0600

    refactor: use indent-blankline

--- nvim/draft/plugins/simple-indent-plugin.lua
+++ nvim/draft/plugins/simple-indent-plugin.lua
@@ -2,7 +2,6 @@ return {
   'lucastavaresa/simpleIndentGuides.nvim',
   config = function()
     vim.opt.list = true -- enable in all buffers
-    -- require("simpleIndentGuides").setup("│", "·")
-    require("simpleIndentGuides").setup("│", " ")
+    require("simpleIndentGuides").setup("│", "·")
   end
 }
