e8a05e6d635dc03ce64d362eb3bbce3ebacf90fe
Sergio Bernal <sergio.bernal@telus.com>
2024-02-09 17:57:14 -0600

    fix: rollback to cokeline

--- nvim/lua/plugins/cokeline.lua
+++ nvim/lua/plugins/cokeline.lua
@@ -5,5 +5,23 @@ return {
     "nvim-tree/nvim-web-devicons", -- If you want devicons
     "stevearc/resession.nvim"      -- Optional, for persistent history
   },
-  config = true
+  config = function()
+    local get_hex = require('cokeline.hlgroups').get_hl_attr
+    local yellow = vim.g.terminal_color_3
+    require("cokeline").setup({
+      sidebar = {
+        filetype = { 'NvimTree', 'neo-tree' },
+        components = {
+          {
+            text = function(buf)
+              return buf.filetype
+            end,
+            fg = yellow,
+            bg = function() return get_hex('NvimTreeNormal', 'bg') end,
+            bold = true,
+          },
+        }
+      },
+    })
+  end
 }
