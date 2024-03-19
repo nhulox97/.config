5a65d5c34c135a38eefbc181473398810a00c3a6
Sergio Bernal <sergio.bernal@telus.com>
2024-03-19 16:28:01 -0600

    refactor(plugins): update ui/ux for noice and toggleterm

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -5,30 +5,43 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function()
-    require("noice").setup {
+    require("noice").setup({
       lsp = {
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
           ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
           ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
         },
       },
+      presets = {
+        bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = false,       -- add a border to hover docs and signature help
+      },
       views = {
-        notify = {
-          enabled = true,
-        },
+        -- Clean cmdline_popup + palette
         cmdline_popup = {
           position = {
-            row = 10,
+            row = 6,
             col = "50%",
           },
+          border = {
+            style = "none",
+            padding = { 1, 2 },
+          },
           size = {
-            width = 60,
+            min_width = 60,
+            width = "auto",
             height = "auto",
           },
+          win_options = {
+            winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "FloatBorder" },
+          },
         },
-        popupmenu = {
+        cmdline_popupmenu = {
           relative = "editor",
           position = {
             row = 8,
@@ -36,24 +49,33 @@ return {
           },
           size = {
             width = 60,
-            height = 10,
+            height = "auto",
+            max_height = 15,
           },
           border = {
-            style = "rounded",
-            padding = { 0, 1 },
+            style = "none",
+            padding = { 0, 2 },
           },
           win_options = {
-            winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
+            winhighlight = { NormalFloat = "NormalFloat", FloatBorder = "NoiceCmdlinePopupBorder" },
+          },
+        },
+        hover = {
+          border = {
+            style = "single",
+          },
+        },
+        confirm = {
+          border = {
+            style = "single",
+          },
+        },
+        popup = {
+          border = {
+            style = "single",
           },
         },
       },
-
-      -- you can enable a preset for easier configuration
-      presets = {
-        long_message_to_split = true, -- long messages will be sent to a split
-        lsp_doc_border = false,       -- add a border to hover docs and signature help
-        inc_rename = true
-      },
-    }
+    })
   end
 }
