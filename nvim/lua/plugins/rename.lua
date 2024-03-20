1d097c7b4c33647e93ea8d5061b9c7c811e06359
Sergio Bernal <sergiobernal909@gmail.com>
2024-03-20 14:37:16 -0600

    refactor(renamer): update renamer plugin

--- nvim/lua/plugins/rename.lua
+++ nvim/lua/plugins/rename.lua
@@ -1,10 +1,27 @@
+---@diagnostic disable: missing-fields
 return {
-  'smjonas/inc-rename.nvim',
+  'filipdutescu/renamer.nvim',
+  branch = 'master',
+  dependencies = { { 'nvim-lua/plenary.nvim' } },
+  keys = {
+    { '<leader>rn', '<cmd>lua require("renamer").rename()<cr>', { noremap = true, silent = true } },
+  },
   config = function()
-    require("inc_rename").setup()
-
-    vim.keymap.set("n", "<leader>rn", function()
-      return ":IncRename " .. vim.fn.expand("<cword>")
-    end, { expr = true })
+    require("renamer").setup({
+      title = 'renamer',
+      padding = {
+        top = 0,
+        left = 0,
+        bottom = 0,
+        right = 0,
+      },
+      min_width = 15,
+      max_width = 45,
+      border = true,
+      border_chars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+      show_refs = true,
+      with_qf_list = true,
+      with_popup = true,
+    })
   end
 }
