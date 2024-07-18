9bdc44adf299075d18c343c5af967004d2e9ae59
Sergio Bernal <sergiobernal909@gmail.com>
2024-07-17 19:13:52 -0600

    ui(wezterm): change font family

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,9 +6,12 @@ config.color_scheme = 'Catppuccin Mocha'
 
 config.enable_tab_bar = false
 
-config.font = wezterm.font('CaskaydiaCove Nerd Font Propo', { weight = 'Medium' })
-config.line_height = 1.54
-config.font_size = 15
+-- config.font = wezterm.font('CaskaydiaCove Nerd Font Propo', { weight = 'Medium' })
+-- config.line_height = 1.54
+-- config.font_size = 15
+config.font = wezterm.font('Agave Nerd Font', { weight = 'Medium' })
+config.line_height = 1.6
+config.font_size = 16.5
 
 config.window_padding = {
   left = 3,
