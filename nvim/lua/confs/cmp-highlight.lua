e704ad7a93cdf10d41730b691547ecf2d47d9066
Sergio Bernal <sergiobernal909@gmail.com>
2024-10-02 12:00:29 -0600

    refactor: add new color scheme for cmp highlights

--- nvim/lua/confs/cmp-highlight.lua
+++ nvim/lua/confs/cmp-highlight.lua
@@ -2,24 +2,44 @@ local dark_colors = require("catppuccin.palettes").get_palette "mocha"
 local light_colors = require("catppuccin.palettes").get_palette "latte"
 
 local colors = {
-  bg = dark_colors.crust,
-  bg1 = dark_colors.mantle,
-  fg = dark_colors.text,
-  fg1 = light_colors.overlay1,
-  fg2 = dark_colors.surface0,
-  fg3 = dark_colors.subtext1,
-  rbg = dark_colors.red,
-  rfg = dark_colors.rosewater,
-  vbg = dark_colors.mauve,
-  lfg = light_colors.lavender,
-  sfg = dark_colors.sapphire,
-  yfg = dark_colors.yellow,
-  ybg = light_colors.yellow,
-  gbg = dark_colors.green,
-  yfg1 = light_colors.rosewater,
-  bbg = light_colors.lavender,
-  cbg = light_colors.teal
+  bg   = '#011627', -- Dark background
+  bg1  = '#001122', -- Slightly lighter background
+  fg   = '#d6deeb', -- Light foreground text for contrast
+  fg1  = '#c5e4fd', -- Lighter text/overlay
+  fg2  = '#8892b0', -- Surface color with more contrast from bg
+  fg3  = '#637777', -- Subtext, less contrast than fg but distinct
+  rbg  = '#ef5350', -- Red background
+  rfg  = '#ff5370', -- Slightly brighter red for text
+  vbg  = '#c792ea', -- Mauve (purple background)
+  lfg  = '#82aaff', -- Light lavender/blue foreground
+  sfg  = '#7fdbca', -- Sapphire (cyan)
+  yfg  = '#ffeb95', -- Yellow foreground
+  ybg  = '#f6d32d', -- Yellow background (more contrast)
+  gbg  = '#22da6e', -- Green background
+  yfg1 = '#ecc48d', -- Light rosewater
+  bbg  = '#82aaff', -- Blue background (lavender)
+  cbg  = '#21c7a8', -- Teal background
 }
+
+-- local colors = {
+--   bg = dark_colors.crust,
+--   bg1 = dark_colors.mantle,
+--   fg = dark_colors.text,
+--   fg1 = light_colors.overlay1,
+--   fg2 = dark_colors.surface0,
+--   fg3 = dark_colors.subtext1,
+--   rbg = dark_colors.red,
+--   rfg = dark_colors.rosewater,
+--   vbg = dark_colors.mauve,
+--   lfg = light_colors.lavender,
+--   sfg = dark_colors.sapphire,
+--   yfg = dark_colors.yellow,
+--   ybg = light_colors.yellow,
+--   gbg = dark_colors.green,
+--   yfg1 = light_colors.rosewater,
+--   bbg = light_colors.lavender,
+--   cbg = light_colors.teal
+-- }
 
 
 vim.api.nvim_set_hl(0, "CmpDoc", { bg = colors.bg, fg = colors.fg })
@@ -36,9 +56,9 @@ vim.api.nvim_set_hl(0, "CmpItemKindField", { fg = colors.rfg, bg = colors.rbg })
 vim.api.nvim_set_hl(0, "CmpItemKindProperty", { fg = colors.rfg, bg = colors.rbg })
 vim.api.nvim_set_hl(0, "CmpItemKindEvent", { fg = colors.rfg, bg = colors.rbg })
 
-vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = colors.fg2, bg = colors.gbg })
-vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = colors.fg2, bg = colors.gbg })
-vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = colors.fg2, bg = colors.gbg })
+vim.api.nvim_set_hl(0, "CmpItemKindText", { fg = colors.fg3, bg = colors.gbg })
+vim.api.nvim_set_hl(0, "CmpItemKindEnum", { fg = colors.fg3, bg = colors.gbg })
+vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { fg = colors.fg3, bg = colors.gbg })
 
 vim.api.nvim_set_hl(0, "CmpItemKindConstant", { fg = colors.yfg, bg = colors.ybg })
 vim.api.nvim_set_hl(0, "CmpItemKindConstructor", { fg = colors.yfg, bg = colors.ybg })
@@ -50,8 +70,8 @@ vim.api.nvim_set_hl(0, "CmpItemKindClass", { fg = colors.fg2, bg = colors.vbg })
 vim.api.nvim_set_hl(0, "CmpItemKindModule", { fg = colors.fg2, bg = colors.vbg })
 vim.api.nvim_set_hl(0, "CmpItemKindOperator", { fg = colors.fg2, bg = colors.vbg })
 
-vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = colors.fg, bg = colors.fg1 })
-vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = colors.fg, bg = colors.fg1 })
+vim.api.nvim_set_hl(0, "CmpItemKindVariable", { fg = colors.fg3, bg = colors.fg1 })
+vim.api.nvim_set_hl(0, "CmpItemKindFile", { fg = colors.fg3, bg = colors.fg1 })
 
 vim.api.nvim_set_hl(0, "CmpItemKindUnit", { fg = colors.rfg, bg = colors.yfg1 })
 vim.api.nvim_set_hl(0, "CmpItemKindSnippet", { fg = colors.rfg, bg = colors.yfg1 })
